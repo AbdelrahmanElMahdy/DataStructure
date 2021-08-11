1929211c17e496a3fb869c25540e93200bd63b20
abdelrahmanelmahdy <a.b.mahdey@gmail.com>
2021-08-11 12:05:54 +0200

    header: insert in  BinaryTree body:implementing inserting BinaryTree footer:MHDI

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -19,4 +19,27 @@ public class BinaryTree {
         this.root=new Node(value);
     }
 
+    public void insert(int value){
+        Node crNode= root;
+        boolean isRight= crNode.value < value;
+
+        while( ( crNode.right != null && isRight )  || ( crNode.left != null && !isRight ) ){
+            if( value > crNode.value )
+                crNode= crNode.right;
+    
+            else if( value < crNode.value )
+                crNode= crNode.left;            
+            
+            isRight= value > crNode.value;
+}
+
+        if (isRight)
+            crNode.right=new Node(value);
+        else
+        crNode.left=new Node(value);
+
+}
+    
+
+
 }
